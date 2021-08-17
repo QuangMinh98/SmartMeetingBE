6ff3780fc547ac023811b913d19fd1823ce3b960
Minh Do <quangminh.bd01@gmail.com>
2021-08-17 22:26:08 +0700

    set up

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, () => {
+    console.log('listening on port ' + port);
+  });
 }
 bootstrap();

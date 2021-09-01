bf1429a09ad25816a7c813e5fcaca21e92b7346d
Minh Do <quangminh.bd01@gmail.com>
2021-09-01 11:15:25 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
+    res.header("Access-Control-Expose-Headers", "x-auth-token");
+    next();
+  });
+
   // app.enableCors({
   //   origin: '*',
   //   methods: 'GET, PUT, POST, DELETE',
